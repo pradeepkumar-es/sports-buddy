34ebc179e10e70ac54e2084cee6eacd6abedc560
pradeepkumar-es <createthefuture121@gmail.com>
2024-01-05 16:02:10 +0530

    bug is fixed

--- src/user/ManageSports.js
+++ src/user/ManageSports.js
@@ -1,13 +1,12 @@
 import LogOut from '../LogOut'
 import { app } from '../firebase'
-// import { Link } from 'react-router-dom'
 import React from 'react'
 function ManageSports () {
   return (
     <div>
       Manage Sports
-      <LogOut/>
-      {/* <button><Link to='/logout'>Log Out</Link></button> */}
+      {/* Log Out component */}
+      <LogOut/>  
     </div>
   )
 }
