1d7ddb0475e90fe5965119c5faa963d79c56150f
pradeepkumar-es <createthefuture121@gmail.com>
2024-01-05 08:08:08 +0530

    manage sorts updated

--- src/user/ManageSports.js
+++ src/user/ManageSports.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { app } from '../firebase'
+import { database } from '../firebase'
+import { collection, addDoc } from 'firebase/firestore'
+import { useState } from 'react'
 
 function ManageSports() {
+  const [sport , setSport] = useState({})
+  const collectionRef = collection(database, 'userSportsManage')
+  const handleInput=()=>{
+    
+  }
+  const handleAdd=async()=>{
+  await addDoc(collectionRef, {
+    sportsName: sport
+  })
+  }
+
   return (
     <div>
-      Manage Sports
+      Manage Sports <br />
+      <input type="text" onClick={handleInput}/>
+      <button onClick={()=>handleAdd}>Add Sports</button>
+      <button>Delete Sports</button>
     </div>
   )
 }
