9317d197251927eda007654b04fe334013993bd8
pradeepkumar-es <createthefuture121@gmail.com>
2024-01-05 17:25:05 +0530

    Adding user sport data to firebase

--- src/user/ManageSports.js
+++ src/user/ManageSports.js
@@ -1,10 +1,32 @@
 import LogOut from '../LogOut'
-import { app } from '../firebase'
-import React from 'react'
+import { app, database } from '../firebase'
+import React, { useState } from 'react'
+import {collection, addDoc} from  "firebase/firestore"
 function ManageSports () {
+  const [data, setData] = useState({})
+  const handleInput=(event)=>{
+  const newItem ={[event.target.name] :(event.target.value)}
+  setData({...data, ...newItem})
+  }
+  console.log(data.sportEvent)
+  const collectionRef = collection(database, "userSports")
+  const handleAdd=()=>{
+    addDoc(collectionRef, {
+      sportEvent: data.sportEvent
+    })
+    .then(()=>{
+      alert("Sport added successfully")
+    })
+    .catch((err)=>{
+      alert(err.message)
+    })
+  }
+  
   return (
     <div>
-      Manage Sports
+      <h4 className="managesport">Manage Sports</h4>
+      <input type="text" name="sportEvent" id="sportEvent" placeholder='Sports Name' onChange={handleInput}/>
+      <button onClick={handleAdd}>Add Sport</button>
       {/* Log Out component */}
       <LogOut/>  
     </div>
